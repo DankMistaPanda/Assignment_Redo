157cc08592a91883d19d1fed1698092baf475dea
Cyrus Mirzaie Damabi <70719496+DankMistaPanda@users.noreply.github.com>
2026-04-27 21:46:43 -0500

    Second commit for Question 2. Added comments

--- src/WeightedShortestPath.java
+++ src/WeightedShortestPath.java
@@ -4,9 +4,7 @@ Write a program that takes the adjacency matrix representation of a graph with n
 a destination node as input and returns the shortest path from start to target using Dijkstra.
  */
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -14,19 +12,19 @@ public class WeightedShortestPath {
     public static List<Integer> shortestPath(int[][] graph, int start, int target){
         int n = graph.length;//Number of Nodes
 
-        boolean[] visited = new boolean[n];//Keeps track of nodes already seen 7 prevents looping forever
-        int[] distance = new int[n];
-        int[] parent = new int[n];
+        boolean[] visited = new boolean[n];//Keeps track of nodes already seen & prevents looping forever
+        int[] distance = new int[n];//Stores distance from start node to all other nodes
+        int[] parent = new int[n];//Tracks previous node of each node.
 
         Arrays.fill(distance, Integer.MAX_VALUE);//Keeps track of where each node came from
-        Arrays.fill(parent, -1);
+        Arrays.fill(parent, -1);//Fills parent array with -1
 
         distance[start] = 0;
 
         for (int count = 0; count < n; count++){//Loops until no nodes remain for exploration
-            int current = getSmallestUnvisited(distance, visited);//Takes next node out of the queue
+            int current = getSmallestUnvisited(distance, visited);//Chooses unvisited node with shortest distance
 
-            if (current == target){
+            if (current == target){//If node is the target, stop
                 break;
             }
 
@@ -34,11 +32,11 @@ public class WeightedShortestPath {
 
             for (int neighbor = 0; neighbor < n; neighbor++){//Checks every possible neighbor
                 if(graph[current][neighbor] > 0 && !visited[neighbor]){//If current is connected to neighbor AND neighbor has not already been visitied
-                    int newDistance = distance[current] + graph[current][neighbor];
+                    int newDistance = distance[current] + graph[current][neighbor];//Finds cost of reaching neighbor from current node
 
-                    if(newDistance < distance[neighbor]){
-                        distance[neighbor] = newDistance;
-                        parent[neighbor] = current;
+                    if(newDistance < distance[neighbor]){//Checks if new path is better than old path
+                        distance[neighbor] = newDistance;//Updates the best distance to the neighbor
+                        parent[neighbor] = current; //keeps track of path to neighbor
                     }
                 }
             }
@@ -59,17 +57,18 @@ public class WeightedShortestPath {
         return path;
     }//end shortestPath
 
+    //Finds unvisited node with shortest distance
     private static int getSmallestUnvisited(int[] distance, boolean[] visited){
-        int smallestDistance = Integer.MAX_VALUE;
-        int smallestNode = -1;
+        int smallestDistance = Integer.MAX_VALUE; //Starts smallest distance at "infinity"
+        int smallestNode = -1; //Starts with no node
 
-        for (int i = 0; i < distance.length; i++){
-            if(!visited[i] && distance[i] < smallestDistance){
-                smallestDistance = distance[i];
-                smallestNode = i;
+        for (int i = 0; i < distance.length; i++){//Loops through every node.
+            if(!visited[i] && distance[i] < smallestDistance){//checks if node i has not been visited & is its distance smaller than the current smallest distance
+                smallestDistance = distance[i];//updates the smallest distance found so far
+                smallestNode = i; //Stores which node has the smallest distance.
             }
         }
-        return smallestNode;
+        return smallestNode; //Returns the closest unvisited node
     }
 
     public static void main(String[] args){
